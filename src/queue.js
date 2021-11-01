19c26b97e6bc4aa6fe31deb75fc8336cbc2c7b37
Max Heilmann <support@matix-media.net>
2021-11-01 17:58:40 +0100

    Made queue a class

--- src/queue.js
+++ src/queue.js
@@ -1,41 +1,41 @@
-const queue = [];
+module.exports = class Queue {
+    _queue = [];
 
-async function queueInterval() {
-    if (queue.length > 0) {
-        const element = queue[0];
-        await element[0](...element[1]);
-        queue.shift();
+    async _queueInterval() {
+        if (queue.length > 0) {
+            const element = queue[0];
+            await element[0](...element[1]);
+            queue.shift();
+        }
+
+        this._queueInterval();
     }
 
-    queueInterval();
-}
-
-module.exports = {
     /**
      * Add a method to the queue
      * @param {() => Promise<any>} method The method that gets executed
      * @param {any[]} parameters Parameters that are passed to the method when executed
      */
     add(method, parameters = []) {
-        queue.push([method, parameters]);
-    },
+        this._queue.push([method, parameters]);
+    }
     /**
      * Start the queue
      */
     start() {
-        queueInterval();
-    },
+        this._queueInterval();
+    }
 
     /**
      * Let the queue wait before executing next method
      * @param {number} milliseconds Milliseconds to wait before next execution
      */
     timeout(milliseconds) {
-        queue.unshift([
+        this._queue.unshift([
             async () => {
                 await new Promise((resolve) => setTimeout(() => resolve(), milliseconds));
             },
             [],
         ]);
-    },
+    }
 };
