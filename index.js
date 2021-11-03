135b5342ab7a5181bd4f3b1539f4524b38806de7
Max Heilmann <support@matix-media.net>
2021-11-03 20:53:42 +0100

    Slight changes

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const EmbedBuilder = require("./src/EmbedBuilder");
 const config = require("./config.json");
 const token = config.token;
 const activities = config.activities;
+const testGuild = config._test_guild;
 const queue = new Queue();
 
 const client = new Discord.Client({
@@ -18,10 +19,8 @@ const client = new Discord.Client({
 
 client.on("messageCreate", MessageHandler.messageCreate);
 
-client.on("ready", () => {
+client.on("ready", async () => {
     console.log(`Logged in as ${client.user.tag}!`);
-
-    MessageHandler.init(queue);
 
     const clientIcon = client.user.avatar
         ? client.user.avatar
@@ -29,10 +28,15 @@ client.on("ready", () => {
 
     EmbedBuilder.setDefault("footer", client.user.username);
     EmbedBuilder.setDefault("footerIcon", clientIcon);
+    EmbedBuilder.setDefault("color", config.message_color);
+
+    MessageHandler.init(queue);
+
+    console.log("Registering commands...");
+    await CommandHandler.registerCommands(client, testGuild);
+    console.log("Commands registered");
 
     function newAct() {
-        // generate random number between 1 and list length.
-
         const randomIndex = Math.floor(Math.random() * (activities.length - 1) + 1);
         const newActivity = activities[randomIndex];
 
@@ -44,63 +48,7 @@ client.on("ready", () => {
     }, 60000 * 30);
 });
 
-/*
-    client.on("messageCreate", (message) => {
-        if (message.author.bot || message.author.id == client.user.id || !message) return;
-
-        const mess = message.cleanContent;
-
-        if (mess.length < 6 || !mess.includes(" ")) {
-            return;
-        } else if (!mess.startsWith(prefix)) {
-            const L = LanguageDetect.detect(mess, 1);
-
-            if (L.length > 0 && L[0].length > 0 && L[0][0] == "english") return;
-
-            // Translate message
-            translateMessage(message, mess);
-        } else {
-            const args = mess.trim().split(/ +/g);
-            const cmd = args[0].slice(prefix.length).toLowerCase(); // case INsensitive, without prefix
-            args.shift();
-            if (cmd == "info") {
-                return message.lineReplyNoMention(
-                    "Thanks you ri1_ for the code used in the translate bot!"
-                );
-            }
-            if (cmd == "help") {
-                return message.lineReplyNoMention(
-                    prefix +
-                        "help: For this menu\n" +
-                        prefix +
-                        "info: For info about the creator\n" +
-                        prefix +
-                        "detect: Let me see if I can determine this strange set of data?\n" +
-                        prefix +
-                        "[language code]: to have me translate your text into that language"
-                );
-            }
-            if (cmd == "detect") {
-                const r = LanguageDetect.detect(args.join(" "), 5);
-                var res = "Let's see what I think it is\n```\n";
-
-                r.forEach((L) => {
-                    res += L[0] + ":" + Math.round(L[1] * 100.0) + "%\n";
-                });
-                res += "```";
-                return message.lineReplyNoMention(res);
-            }
-            const output = mess.replace(prefix + cmd, "");
-            translate(output, { to: cmd })
-                .then((res) => {
-                    message.lineReplyNoMention(filter.clean(res.text)); // OUTPUT: You are amazing!
-                })
-                .catch((err) => {
-                    console.error(err);
-                });
-        }
-    });
-*/
-
 queue.start();
+
+console.log("Logging in...");
 client.login(token);
